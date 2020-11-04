443b6670a29558c3adc2a6a7b932072785d7e4e1
cbxcube <cub.bed@gmail.com>
2020-11-04 04:33:15 +0100

    add new

--- working/lcd_measure_lcd/lcd_measure_lcd.ino
+++ working/lcd_measure_lcd/lcd_measure_lcd.ino
@@ -1,17 +1,27 @@
 // Ultrazvukový modul HY-SRF05 pro měření vzdálenosti
 
-// připojení potřebné knihovny
+// load library for ultrasound
 #include <NewPing.h>
-// nastavení propojovacích pinů
+// load library for lcd 16x2 i2c
+#include <LiquidCrystal_I2C.h>
+
+// nastavení propojovacích pinů - ultrazvuk
 #define pinTrigger    3
 #define pinEcho       4
 #define maxVzdalenost 450
-// inicializace měřícího modulu z knihovny
+
+// inicializace měřícího modulu ultrazvuku z knihovny
 NewPing sonar(pinTrigger, pinEcho, maxVzdalenost);
+// inicializace displaya na I2C adrese 0x27
+// 0x27 and 16 characters in 2 lines
+LiquidCrystal_I2C lcd(0x27,16,2);  
 
 void setup() {
   // zahájení komunikace po sériové lince
   Serial.begin(9600);
+  // inicializacia displaya
+  lcd.init();// initialise display
+  lcd.backlight(); // backlight on
 }
 
 void loop() {
@@ -36,11 +46,26 @@ void loop() {
     Serial.print("Vzdalenost mezi senzorem a predmetem je ");
     Serial.print(vzdalenost);
     Serial.println(" cm.");
+    // print na display
+    lcd.setCursor(0,0);
+    lcd.print("Vzdialenost je:");
+    // lcd.setCursor(11,0);
+    // lcd.print(temperatureF);
+    lcd.setCursor(0,1);
+    lcd.print(vzdalenost);
+    lcd.println(" cm."            );
+    // lcd.setCursor(11,1);
+    // lcd.print(temperatureC);
   }
   // pokud byla detekována vzdálenost 0, je předmět mimo měřící rozsah,
   // tedy příliš blízko nebo naopak daleko
   else {
     Serial.println("Vzdalenost mezi senzorem a predmetem je mimo merici rozsah.");
+    lcd.setCursor(0,0);
+    lcd.print(" Predmet mimo   ");
+    lcd.setCursor(0,1);
+    lcd.print(" meraci rozsah !");
     delay(500);
+    
   }
 }
